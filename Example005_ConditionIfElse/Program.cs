929e69b849cf73f9de8b1eee6da2589b4b34a1b4
f1ga <welcometotheclubbuddy89@gmail.com>
2023-07-30 16:17:17 +0300

    ex005_if_else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine ();
+
+if (username.ToLower()=="дима")
+{
+    Console.WriteLine("Ура, это же ДИМА!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
